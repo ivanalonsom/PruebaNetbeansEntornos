5d313e6f98c8c34213f035f8f2237512d2c6c09b
Iván Alonso <ivan.alomun@educa.jcyl.es>
2024-04-25 16:59:26 +0200

    Primer comit del proyecto

---
+++ src/main/java/com/mycompany/proyectoentornoseva3/ProyectoEntornosEVA3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoentornoseva3;
+
+/**
+ *
+ * @author admin
+ */
+public class ProyectoEntornosEVA3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
